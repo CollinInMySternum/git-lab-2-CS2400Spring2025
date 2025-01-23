787a8454fc33045f5ad02097e36490d29b5021fc
CollinInMySternum <projectunkowngame@gmail.com>
2025-01-22 19:24:00 -0500

    Initial Push: Git Lab

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Collin Blumenauer
+ *        Date: 1/22/2025
+ * Description: Git Lab Test
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
